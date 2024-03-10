206a3afd378e88efd6a6dd4c2449058959102aab
lanni0619 <lanni0619@gmail.com>
2024-03-10 15:20:37 +0800

    Class2-7 refactor: querySelector & closest

--- todolist/index.js
+++ todolist/index.js
@@ -1,89 +1,94 @@
 function inputBtn() {
-    const listText = document.getElementById("input").value;
-    var list = document.getElementById("list");
-    var elemSpan = document.createElement('span');
-    var elemLi = document.createElement('li');
-    var elemBtn = document.createElement('button');
-    var doneBtn = document.createElement('button');
-    var statusText = document.createElement('span');
+    const listText = document.querySelector('#input').value;
+    let list = document.querySelector('#list');
+    let elemSpan = document.createElement('span');
+    let elemLi = document.createElement('li');
+    let elemBtn = document.createElement('button');
+    let doneBtn = document.createElement('button');
+    let statusText = document.createElement('p');
 
-    let prior = document.getElementById('priorSelect');
+    let prior = document.querySelector('#priorSelect');
     let index = prior.selectedIndex;
     let priorText = prior.options[index].text;
 
-    elemSpan.textContent = listText;
-    doneBtn.textContent = 'Done';
-    elemBtn.textContent = "Del";
-    statusText.textContent = '(Check)';
-    statusText.style.color = 'lightgreen';
-    statusText.style.display = 'none';
-
-    list.append(elemLi);
-    elemLi.append(elemSpan);
-    elemLi.append(statusText);
-    elemLi.append(doneBtn);
-    elemLi.append(elemBtn);
-
-    elemBtn.onclick = delBtn;
-    doneBtn.onclick = statusCheck;
-
-    if (priorText == "Important") {
-        elemSpan.className = 'important';
-    }
-
-    if (priorText == "Urgent") {
-        elemSpan.className = 'urgent';
+    if(listText==""){
+        alert("您未輸入待辦事項:(");
+    }else{    
+        elemSpan.textContent = listText;
+        doneBtn.textContent = '[標記完成]';
+        elemBtn.textContent = 'Delete';
+        statusText.textContent = '(已完成)';
+        statusText.className = 'statusText';
+        doneBtn.className = 'doneBtn';
+    
+        list.append(elemLi);
+        elemLi.append(elemSpan,statusText,doneBtn,elemBtn);
+    
+        elemBtn.onclick = delBtn;
+        doneBtn.onclick = statusCheck;
+    
+        if (priorText == 'Important') {
+            elemSpan.className = 'important';
+        }
+    
+        if (priorText == 'Urgent') {
+            elemSpan.className = 'urgent';
+        }
     }
 }
 
 function delBtn() {
-    event.target.parentElement.remove();
+    event.target.closest('li').remove();
 }
 
 function exportBtn() {
-    let listText = document.getElementById('list').children;
+    let listText = document.querySelectorAll('li span');
     let exportText = "待辦事項: ";
     let num = 1;
 
-    for (var x of listText) {
-        if (x.children[0].className == 'important') {
-            exportText = exportText + '(' + num.toString() + ')*' + x.children[0].textContent + "* ";
-        } else if (x.children[0].className == 'urgent') {
-            exportText = exportText + '(' + num.toString() + ')**' + x.children[0].textContent + "** ";
-        } else {
-            exportText = exportText + '(' + num.toString() + ')' + x.children[0].textContent + " ";
+    if(listText.length==0){
+        alert("無清單可匯出:(");
+    }else{
+        for (var x of listText) {
+            if (x.className == 'important') {
+                exportText = exportText + '(' + num.toString() + ')*' + x.textContent + "* ";
+            } else if (x.className == 'urgent') {
+                exportText = exportText + '(' + num.toString() + ')**' + x.textContent + "** ";
+            } else {
+                exportText = exportText + '(' + num.toString() + ')' + x.textContent + " ";
+            }
+            num = num + 1;
         }
-        num = num + 1;
+        alert(exportText);
     }
-    alert(exportText);
 }
 function changeInputColor() {
-    let elemInput = document.getElementById('input');
-    elemInput.classList.remove('orangeFontClass');
-    elemInput.classList.remove('redFontClass');
-    elemInput.classList.remove('blackFontClass');
+    let elemInput = document.querySelector('#input');
+    elemInput.classList.remove('orangeFont');
+    elemInput.classList.remove('redFont');
+    elemInput.classList.remove('blackFont');
     switch (event.target.value) {
         case 'important':
-            elemInput.className = elemInput.className + 'orangeFontClass';
+            elemInput.className = elemInput.className + 'orangeFont';
             break;
         case 'urgent':
-            elemInput.className = elemInput.className + 'redFontClass';
+            elemInput.className = elemInput.className + 'redFont';
             break;
         default:
-            elemInput.className = elemInput.className + 'blackFontClass';
+            elemInput.className = elemInput.className + 'blackFont';
             break;
     }
 }
 
 function statusCheck(){
-    let x = event.target.textContent;
-    let y = event.target.parentElement;
-    console.log(x);
-    if(event.target.textContent=='Done'){
-        event.target.textContent = 'Undone';
-        event.target.parentElement.children[1].style.display='block';
-    }else if (event.target.textContent=='Undone'){
-        event.target.textContent = 'Done';
-        event.target.parentElement.children[1].style.display='none';
+    let x = event.target;
+    let y = x.closest('li');
+    let z = y.querySelector('.statusText');
+    if(x.textContent=='[標記完成]'){
+        x.textContent = '[取消標記]';       
+        z.style.display='block';
+    }else if (x.textContent=='[取消標記]'){
+        x.textContent = '[標記完成]';
+        z.style.display='none';
     }
 }
