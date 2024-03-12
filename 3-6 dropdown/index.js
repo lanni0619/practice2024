8118bb4208c825bd3002831ee138b755851122e5
lanni0619 <lanni0619@gmail.com>
2024-03-12 19:29:26 +0800

    add two dropdown function
    class: all-commodity, all-brand,

--- 3-6 dropdown/index.js
+++ 3-6 dropdown/index.js
@@ -1,15 +1,41 @@
+let allCommodity = document.querySelector('.all-commodity');
+let allBrand = document.querySelector('.all-brand');
 let aboutUs = document.querySelector('.about-us');
-let subIndex = document.querySelector('.sub-index');
+let subIndex = document.querySelectorAll('.sub-index');
 
+allCommodity.onclick = dropdown;
+allBrand.onclick = dropdown;
 aboutUs.onclick = dropdown;
 window.onclick = closeDrop;
 
-function dropdown() {
-    subIndex.classList.toggle('show');
+function dropdown(event) {
+    switch (event.target.closest('a').classList.value) {
+        case 'all-commodity':
+            subIndex[0].classList.toggle('show');
+            break;
+
+        case 'all-brand':
+            subIndex[1].classList.toggle('show');
+            break;
+
+        case 'about-us':
+            subIndex[2].classList.toggle('show');
+
+        default:
+            break;
+    }
 }
 
 function closeDrop(event) {
+    if (event.target != allCommodity) {
+        subIndex[0].classList.remove('show');
+    }
+
+    if (event.target != allBrand) {
+        subIndex[1].classList.remove('show');
+    }
+
     if (event.target != aboutUs) {
-        subIndex.classList.remove('show');
+        subIndex[2].classList.remove('show');
     }
 }
