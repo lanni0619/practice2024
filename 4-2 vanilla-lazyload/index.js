3f95c58b498d7a9d9d96dc8df68a5cd9d1c9031e
lanni0619 <lanni0619@gmail.com>
2024-03-14 13:11:07 +0800

    +add 4-2 lazyload project
    1. 4-2: use multi-img to demo lazy load benefit
    
    2. move some folder to (1 & 2 side project)

---
+++ 4-2 vanilla-lazyload/index.js
@@ -0,0 +1,5 @@
+var lazyLazy = new LazyLoad({
+    // elements_selector: ".horizContainer",
+    // callback_enter: initOneLazyLoad,
+    // unobserve_entered: true // Stop observing .horizContainer(s) after they entered
+});
