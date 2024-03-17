e3ac82d64ba02418efd8e449914196927b33b213
lanni0619 <lanni0619@gmail.com>
2024-03-17 17:03:38 +0800

    +add show product description in modal
    1. catch(error)
    2. use fetch delete data
    3. use dataset to store data

---
+++ 5-4 AJAX/README.md
@@ -0,0 +1,6 @@
+Note:
+1. learn how to use fetch
+2. arrow function (ES6)
+3. for( x of y) bug: default "var x", hoisting (x宣告為全域)
+4. promise concept
+5. asynchronous
--- 5-4 AJAX/index.js
+++ 5-4 AJAX/index.js
@@ -2,6 +2,13 @@
 //     .then(res => res.json())
 //     .then(json => console.log(json))
 let tagUl = document.getElementById('list');
+let postTag = document.querySelector('.modal#post-modal')
+let closePost = document.querySelector('#post-modal .btn-close');
+let showName = document.getElementById('showName');
+let showClass = document.getElementById('showClass');
+let showExplain = document.getElementById('showExplain');
+let showPhoto = document.getElementById('showPhoto');
+let showPrice = document.getElementById('showPrice');
 
 function load() {
     fetch('https://fakestoreapi.com/products')
@@ -12,19 +19,48 @@ function load() {
                 let spanTag = document.createElement('span');
                 let liTag = document.createElement('li');
                 let buttonTag = document.createElement('button');
-                let imgTag = document.createElement('img');
-                imgTag.src = product.image;
+                let delBtn = document.createElement('button');
+
                 spanTag.append(product.title);
                 buttonTag.append('Details');
                 buttonTag.classList.add('btn' + product.id)
-                liTag.append(spanTag, imgTag, buttonTag);
+                delBtn.append('delete');
+                delBtn.classList.add('delBtn');
+                liTag.append(spanTag, buttonTag, delBtn);
                 tagUl.append(liTag);
 
-                document.querySelector('.btn' + product.id).addEventListener('click', function () {
-                    alert(product.id);
-                    alert(product.category);
-                    alert(product.description);
-                });
+                liTag.dataset.title = product.title;
+                liTag.dataset.category = product.category;
+                liTag.dataset.description = product.description;
+                liTag.dataset.image = product.image;
+                liTag.dataset.price = product.price;
+
+                buttonTag.onclick = postModal;
+
+                delBtn.onclick = () => {
+                    fetch('https://fakestoreapi.com/products/' + product.id, {
+                        method: "DELETE"
+                    })
+                        .then(res => res.json())
+                        .then(json => console.log(json))
+                    event.target.closest('li').remove()
+                }
             }
         })
+        .catch(error => {
+            alert('抱歉，請稍後重新嘗試。');
+        })
+}
+
+function postModal(event) {
+    postTag.style.width = '100%';
+    showName.textContent = event.target.closest('li').dataset.title;
+    showClass.textContent = event.target.closest('li').dataset.category;
+    showExplain.textContent = event.target.closest('li').dataset.description;
+    showPhoto.src = event.target.closest('li').dataset.image;
+    showPrice.textContent = event.target.closest('li').dataset.price;
+}
+
+closePost.onclick = function () {
+    postTag.style.width = '0%';
 }
