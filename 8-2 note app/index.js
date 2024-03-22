80d0572a415823d861e14bb843fbe8cf4d467241
lanni0619 <lanni0619@gmail.com>
2024-03-22 15:17:17 +0800

    +add folder 8-2
    main function is done.
    focus Note function not yet.

--- 8-2 note app/index.js
+++ 8-2 note app/index.js
@@ -4,23 +4,55 @@ createApp({
     data() {
         return {
             notes: [],
+            focusIndex: 0,
+            delBtnIsBlock: false
         };
     },
     methods: {
         newNote() {
-            if (this.$refs.textarea.value != '') {
+            if (this.$refs.textarea.value == '') {
+                alert('Please type your note...');
+
+            } else if (!this.focusNote) {
+                // set status
                 this.notes.push(this.$refs.textarea.value);
-                console.log(this.notes);
+            } else if (this.focusNote) {
+                // reset UI
+                this.notes[this.focusIndex] = this.$refs.textarea.value;
                 this.$refs.textarea.value = '';
-            } else {
-                alert('Please type your notes!');
+                this.$refs.newBtn.textContent = '+New';
+                this.$refs.textarea.style.backgroundColor = 'white';
+                // reset status
+                this.focusIndex = 0;
+                this.focusNote = false;
+                this.delBtnIsBlock = false;
             }
         },
         delNote(index) {
             this.notes.splice(index, 1);
+            this.$refs.textarea.value = '';
         },
         modifyNote(index) {
+            //set status
+            this.delBtnIsBlock = true;
+            this.focusNote = true;
+            //set UI
             this.$refs.textarea.value = this.notes[index];
+            this.$refs.textarea.style.backgroundColor = '#A3BE8C';
+            this.$refs.newBtn.textContent = '*Modify';
         }
-    }
+    },
+    watch: {
+        notes: {
+            handler(value) {
+                localStorage.setItem('notesData', JSON.stringify(this.notes));
+            },
+            deep: true,
+        }
+    },
+    mounted() {
+        if (localStorage.getItem('notesData')) {
+            this.notes = JSON.parse(localStorage.getItem('notesData'));
+        }
+    },
 }).mount("#app");
