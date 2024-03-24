438769ce99ebec87edc9ae7a152f35f7189487cb
lanni0619 <lanni0619@gmail.com>
2024-03-24 19:30:06 +0800

    *modify
    del function
    watch function

--- 8-2 note app/index.js
+++ 8-2 note app/index.js
@@ -23,24 +23,21 @@ createApp({
         this.delBtnIsBlock = false;
         this.notes[this.notes.length - 1].focusNote = true;
       } else {
-        let data = {
-          content: null,
-          Index: this.notes.length,
-          focusNote: true,
-        };
         this.notes[this.notes.length - 1].focusNote = false;
-        this.notes.push(data);
+        this.createNewNote();
       }
     },
     modifyNote(note, index) {
-      //set status
-      this.resetFocus();
-      this.notes[index].focusNote = true;
-      this.modifyOn = true;
-      this.modifyIndex = index;
-      //set UI
-      this.delBtnIsBlock = true;
-      this.$refs.newBtn.textContent = "*Modify";
+      if (index != this.notes.length - 1) {
+        //set status
+        this.resetFocus();
+        this.notes[index].focusNote = true;
+        this.modifyOn = true;
+        this.modifyIndex = index;
+        //set UI
+        this.delBtnIsBlock = true;
+        this.$refs.newBtn.textContent = "*Modify";
+      }
     },
     resetFocus() {
       for (index in this.notes) {
@@ -48,30 +45,38 @@ createApp({
       }
     },
     delNote(index) {
-      this.notes.splice(index, 1);
-      this.resetFocus();
-      if (this.notes.length == 0) {
-        let data = {
-          content: null,
-          Index: this.notes.length,
-          focusNote: true,
-        };
-        this.notes.push(data);
+      if (this.notes.length <= 1) {
+        this.notes.splice(index, 1);
+        this.createNewNote();
+      } else if (this.notes.length > 1) {
+        if (index == this.notes.length - 1) {
+          this.notes.splice(index, 1);
+          this.createNewNote();
+        } else {
+          this.notes.splice(index, 1);
+        }
       }
-      // this.notes[this.notes.length - 1].focusNote = true;
+    },
+    createNewNote() {
+      let data = {
+        content: null,
+        Index: this.notes.length,
+        focusNote: true,
+      };
+      this.notes.push(data);
     },
   },
-  // watch: {
-  //   notes: {
-  //     handler(value) {
-  //       localStorage.setItem("notesData", JSON.stringify(this.notes));
-  //     },
-  //     deep: true,
-  //   },
-  // },
-  // mounted() {
-  //   if (localStorage.getItem("notesData")) {
-  //     this.notes = JSON.parse(localStorage.getItem("notesData"));
-  //   }
-  // },
+  watch: {
+    notes: {
+      handler(value) {
+        localStorage.setItem("noteBookData", JSON.stringify(this.notes));
+      },
+      deep: true,
+    },
+  },
+  mounted() {
+    if (localStorage.getItem("noteBookData")) {
+      this.notes = JSON.parse(localStorage.getItem("noteBookData"));
+    }
+  },
 }).mount("#app");
