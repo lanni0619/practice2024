972b4048ca215dcbe3b12ee82bfcb8c81a484a73
lanni0619 <lanni0619@gmail.com>
2024-03-27 11:02:01 +0800

    +add modal function

--- 8-4 calendar/index.js
+++ 8-4 calendar/index.js
@@ -4,36 +4,74 @@ const app = createApp({
   data() {
     return {
       showModal: false,
+      exercises: [],
+      title: '',
+      eventTime: '',
     };
   },
   methods: {
+    openModal() {
+      this.showModal = true;
+    },
     addEvent() {
+      let data = {
+        title: this.title,
+        eventTime: this.eventTime,
+      };
+      this.exercises.push(data);
       this.showModal = false;
+      console.log(this.exercises);
     },
     clsModal() {
       this.showModal = false;
     },
-  },
-  mounxted() {
-    document.addEventListener("DOMContentLoaded", function () {
-      var appEl = document.getElementById("appCalendar");
-      var appCalendar = new FullCalendar.Calendar(appEl, {
+    createCalendar() {
+      let openModal = this.openModal;
+      var calendarEl = document.getElementById('app');
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
         customButtons: {
           myCustomButton: {
-            text: "新增活動",
+            text: '新增活動',
             click: function () {
-              this.showModal = true;
-            },
-          },
+              openModal();
+            }
+          }
         },
         headerToolbar: {
-          left: "prev,next today",
-          center: "title",
-          right: "myCustomButton",
-        },
+          left: 'prev,next today myCustomButton',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }
       });
-      appCalendar.render();
-    });
+      calendar.on('dateClick', function (info) {
+        console.log('clicked on ' + info.dateStr);
+      });
+      calendar.render();
+    },
+  },
+  watch: {
+    // showModal: {
+    //   handler(value) {
+    //     localStorage.setItem("showModal", JSON.stringify(this.showModal))
+    //   },
+    //   deep: true,
+    // }
+  },
+  created() {
+    this.createCalendar();
+  },
+  mounted() {
+    // datetimepicker
+    $.datetimepicker.setLocale('zh');
+    $('#datetimepicker1').datetimepicker();
+
+    // if (localStorage.getItem("split_members")) {
+    //   this.members = JSON.parse(localStorage.getItem("split_members"));
+    // }
+    // if (localStorage.getItem("split_bills")) {
+    //   this.bills = JSON.parse(localStorage.getItem("split_bills"));
+    // }
   },
 });
 
