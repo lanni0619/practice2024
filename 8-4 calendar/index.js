6da0d207dd4bf60558456ef4a03190e007e3cd10
lanni0619 <lanni0619@gmail.com>
2024-03-28 15:44:27 +0800

    +add 8-4 function
    finish homework function

--- 8-4 calendar/index.js
+++ 8-4 calendar/index.js
@@ -2,95 +2,110 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
+      //instance
       calendar: null,
+      // ref
+      calendarApp: null,
+      eventTimeDOM: null,
+      //control modal
       showModal: false,
+      //important data
       exercises: [],
-      title: '',
-      timeMessage: '',
       id: 0,
+      //temporary storage
+      title: null,
+      whatDate: null,
+      whatTime: null,
     };
   },
   methods: {
+    addEvent() {
+      const cld = this.calendar;
+      let timeMessage = this.$refs.eventTimeDOM.value;
+      if (this.title != '' && timeMessage != '') {
+        let data =
+        {
+          id: this.id,
+          title: timeMessage.slice(11, 16) + ' ' + this.title,
+          start: timeMessage.slice(0, 10),
+        };
+        cld.addEvent(data);
+        this.exercises.push(data);
+        this.clsModal();
+        this.id = this.id + 1;
+      } else {
+        this.clsModal();
+        alert('輸入欄位不能空白');
+      }
+    },
+    delExercises(info) {
+      let check = confirm('確定要刪除此筆活動嗎？');
+      if (check) {
+        for (index in this.exercises) {
+          if (this.exercises[index].title = info.event.title) {
+            this.exercises.splice(index, 1);
+          }
+        }
+        info.event.remove();
+      }
+    },
     openModal() {
       this.showModal = true;
     },
-    addEvent() {
-      let data = {
-        id: this.id,
-        title: this.title,
-        start: this.timeMessage,
-      };
-      this.exercises.push(data);
 
-      this.calendar.addEvent({
-        id: this.exercises[this.id].id,
-        title: String(this.exercises[this.id].title),
-        start: '2024/03/25'
-      })
-
-      this.showModal = false;
-      this.id = this.id + 1;
-      // this.calendar.addEvent({
-      //   id: 1,
-      //   title: 'dynamic event',
-      //   start: '2024-03-27',
-      // })
-    },
     clsModal() {
       this.showModal = false;
     },
   },
-  watch: {
-    // showModal: {
-    //   handler(value) {
-    //     localStorage.setItem("showModal", JSON.stringify(this.showModal))
-    //   },
-    //   deep: true,
-    // }
-  },
-  mounted() {
-    const vm = this; // 儲存Vue instance的引用
-    var calendarEl = this.$refs.calendar; // = document.getElementById('#calendar')
-    jQuery.datetimepicker.setLocale('en');
-    const inputElement = this.$refs.datetimepicker;
 
-    // datetimepicker套件可能是直接操作DOM不會經過Vue的系統，即便在DOM加上v-model也不行，因此要手動監聽
-    jQuery(inputElement).datetimepicker({
-      timepicker: false,
-      onChangeDateTime: function (dp, $input) {
-        // vm.updateTimeMessage($input.val());
-        vm.timeMessage = $input.val();
-      }
+  watch: {
+    exercises: {
+      handler(value) {
+        localStorage.setItem("calendarEvents", JSON.stringify(this.exercises))
+      },
+      deep: true,
+    },
+    id: {
+      handler(value) {
+        localStorage.setItem("calendarId", JSON.stringify(this.id))
+      },
+      deep: true,
+    }
+  },
+
+  mounted() {
+    if (localStorage.getItem("calendarEvents")) {
+      this.exercises = JSON.parse(localStorage.getItem("calendarEvents"));
+    }
+
+    if (localStorage.getItem("calendarId")) {
+      this.id = JSON.parse(localStorage.getItem("calendarId"));
+    }
+
+    //沒有支援v-model，但用ref也可以拿到值
+    $(this.$refs.eventTimeDOM).datetimepicker({
+      timepicker: true,
+      format: 'Y-m-d H:i',
+      minDate: 0,
     });
 
-    this.calendar = new FullCalendar.Calendar(calendarEl, {
+    this.calendar = new FullCalendar.Calendar(this.$refs.calendarApp, {
       initialView: 'dayGridMonth',
       customButtons: {
         myCustomButton: {
           text: '新增活動',
-          click: function () {
-            vm.openModal();
-          }
+          click: this.openModal,
         }
       },
       headerToolbar: {
-        left: 'prev,next today myCustomButton',
+        left: '',
         center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        right: 'myCustomButton today prev,next'
       },
-      events: [
-        // {
-        //   id: 'a',
-        //   title: 'my event',
-        //   start: '2024-03-27'
-        // },
-        // {
-        //   id: 'b',
-        //   title: 'my event2',
-        //   start: '2024-03-28'
-        // },
-      ]
+      events: this.exercises,
+      eventClick: this.delExercises,
     });
+
     this.calendar.render();
   },
 });
