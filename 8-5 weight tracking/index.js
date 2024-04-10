0f37ddd97b800a8b964f91204f80676867bd85be
lanni0619 <lanni0619@gmail.com>
2024-04-10 11:07:51 +0800

    add 8-5 function
    add data into chart and update
    sort data by date
    delete data
    css unfinish

--- 8-5 weight tracking/index.js
+++ 8-5 weight tracking/index.js
@@ -1,34 +1,108 @@
 const { createApp } = Vue;
 
 const app = createApp({
+  chartJs: null,
   data() {
     return {
-      chartJs: null,
-      labels: null,
-      data: null,
+      labels: [],
+
+      record: [],
+
+      weights: [],
+      dates: [],
     };
   },
   methods: {
 
+    submitRecord() {
+      let data =
+      {
+        date: this.$refs.inputDate.value,
+        weight: this.$refs.inputWeight.value,
+      }
+      this.record.push(data);
+
+      this.record.sort(function (a, b) {
+        return new Date(a.date) - new Date(b.date);
+      });
+
+      this.dates = [];
+      this.weights = [];
+      for (data of this.record) {
+        this.dates.push(data.date);
+        this.weights.push(data.weight);
+      }
+
+      this.chartJs.data.labels = JSON.parse(JSON.stringify(this.dates))
+      this.chartJs.data.datasets[0].data = JSON.parse(JSON.stringify(this.weights))
+      this.chartJs.update();
+      this.$refs.inputWeight.value = null;
+      this.$refs.inputDate.value = null;
+    },
+
+    del(index) {
+      console.log(index);
+      this.weights.splice(index, 1);
+      this.dates.splice(index, 1);
+      this.record.splice(index, 1);
+      this.chartJs.data.labels = JSON.parse(JSON.stringify(this.dates))
+      this.chartJs.data.datasets[0].data = JSON.parse(JSON.stringify(this.weights))
+      this.chartJs.update();
+    }
+  },
+  watch: {
+    record: {
+      handler(value) {
+        localStorage.setItem("chartRecord", JSON.stringify(this.record))
+      },
+      deep: true,
+    },
+    weights: {
+      handler(value) {
+        localStorage.setItem("chartWeights", JSON.stringify(this.weights))
+      },
+      deep: true,
+    },
+    dates: {
+      handler(value) {
+        localStorage.setItem("chartDates", JSON.stringify(this.dates))
+      },
+      deep: true,
+    }
   },
   mounted() {
-    this.labels = ['Jan', 'Feb', 'Mar', 'April', 'May', 'Jun', 'Jul'];
-    this.data = {
-      labels: this.labels,
-      datasets: [{
-        label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1
-      }]
-    };
+    //沒有支援v-model，但用ref也可以拿到值
+    $(this.$refs.inputDate).datetimepicker({
+      timepicker: false,
+      format: 'Y-m-d',
+      // 'Y-m-d H:i'
+      minDate: 0,
+    });
+
+    if (localStorage.getItem("chartWeights")) {
+      this.weights = JSON.parse(localStorage.getItem("chartWeights"));
+    }
+    if (localStorage.getItem("chartRecord")) {
+      this.record = JSON.parse(localStorage.getItem("chartRecord"));
+    }
+    if (localStorage.getItem("chartDates")) {
+      this.dates = JSON.parse(localStorage.getItem("chartDates"));
+    }
+
     this.chartJs = new Chart(this.$refs.ctx, {
       type: 'line',
-      data: this.data,
-      options: {
-
-      }
+      data: {
+        labels: JSON.parse(JSON.stringify(this.dates)),
+        datasets: [{
+          label: '體重',
+          data: JSON.parse(JSON.stringify(this.weights)),
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1,
+          borderWidth: 1
+        }]
+      },
+      options: {}
     });
   }
 })
